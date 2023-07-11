fce8b08a31b5121495a3d639b3a89d107e3696f1
Zhen Zhang <zhzhn@amazon.com>
2023-07-11 12:05:30 -0700

    config relative line num

--- init.lua
+++ init.lua
@@ -514,3 +514,7 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.o.relativenumber=true
+vim.api.nvim_set_option("clipboard", "unnamed")
+vim.api.nvim_set_keymap("i", "jk", "<esc>", { noremap = true})
