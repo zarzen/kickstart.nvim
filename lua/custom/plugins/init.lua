e057149df9f98a5da3a4f42c59f7a75f891a35f9
Zhen Zhang <zhzhn@amazon.com>
2023-07-16 14:21:00 -0700

    include neogit plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+   'NeogitOrg/neogit',
+    dependencies = {'nvim-lua/plenary.nvim'},
+    config = function ()
+    	require('neogit').setup {}
+    end,
+}
